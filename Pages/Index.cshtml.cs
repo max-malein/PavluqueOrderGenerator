5f0398f56cbb5e6ebc7fb02b752c9d7de68e776f
Max <max@malein.com>
2021-07-17 22:59:01 +0300

    Добавьте файлы проекта.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using PavluqueOrderGenerator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PavluqueOrderGenerator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Console.WriteLine();
+        }
+
+        public void OnPost(Order order)
+        {
+            Console.WriteLine("Yes");
+            Console.ReadLine();
+        }
+    }
+}
